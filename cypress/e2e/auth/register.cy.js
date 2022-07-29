98d7cca407dca09914bd0d8e9b5eb3d43ed0d8d3
bantawa04 <rahunn3@gmail.com>
2022-07-29 13:13:33 +0545

    user register test

---
+++ cypress/e2e/auth/register.cy.js
@@ -0,0 +1,20 @@
+describe('Register new user', () => {
+  it('Register new user and redirect to signup page', () => {
+    //visit baseUrl
+    cy.visit("/");
+    //find li elemnt that contains text "Login" and click it
+    cy.get("li").contains("Signup").click();
+    //confirm/assert the url is changed to  /signup
+    cy.url().should("include", "/signup"); 
+
+    //get input field by id and type given param with delay speed of 100
+    cy.get("#email").type("test@user.com", {delay: 100});
+    cy.get("#password").type("password123", {delay: 100});
+    cy.get("#confirmPassword").type("password123", {delay: 100});
+
+    cy.get(".btn").click();
+
+    //assert signup success and redirect to login
+    cy.url().should("include", "/login"); 
+  })
+})
